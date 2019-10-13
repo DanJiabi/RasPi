c02c900c8f4d2f127b1a11167567306366f333b9
DanJiabi <danjiabi@163.com>
2019-10-13 10:14:17 +0800

    prepare for installing opencv-python in python3

---
+++ pre_opencv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install libjpeg-dev
+sudo apt-get install libtiff5-dev
+sudo apt-get install libpng12-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqtgui4 libqt4-test
+sudo apt-get install libatlas-base-dev
+
